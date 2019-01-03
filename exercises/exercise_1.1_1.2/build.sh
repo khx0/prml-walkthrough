2865156d9af10661481a68015ddb15a62a1db901
Nikolas Schnellbächer <khx0@posteo.net>
2019-01-03 10:15:07 +0100

    updated tex automation scripts

--- exercises/exercise_1.1_1.2/build.sh
+++ exercises/exercise_1.1_1.2/build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
 # build local TeX document from source
-# date: 2018-10-06
+# date: 2019-01-03
 ##########################################################################################
 
-# tex sheet
+# tex main document
 pdflatex main.tex
 bibtex main.aux
 pdflatex main.tex
@@ -14,4 +15,3 @@ pdflatex main.tex
 
 # clean up auxiliary files
 rm *.aux *.log  *.out *.toc *.bbl *.blg *.xwm
-
--- exercises/exercise_1.1_1.2/release.sh
+++ exercises/exercise_1.1_1.2/release.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
 # build and release local TeX document from source with timestamp
-# date: 2018-10-06
+# date: 2019-01-03
 ##########################################################################################
 
-# tex sheet
+# tex main document
 pdflatex main.tex
 bibtex main.aux
 pdflatex main.tex
@@ -20,4 +21,3 @@ outname="PRML_Exercise_1.1_and_1.2_$(date +%Y-%m-%d).pdf"
 echo $outname
 
 mv main.pdf $outname
-
