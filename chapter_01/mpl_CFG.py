be0e4421468b3e0202803dbdda2b0436e7389565
Nikolas Schnellbächer <khx0@posteo.net>
2019-01-27 12:17:42 +0100

    added mpl_CFG.py module

---
+++ chapter_01/mpl_CFG.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+##########################################################################################
+# author: Nikolas Schnellbaecher
+# contact: khx0@posteo.net
+# date: 2019-01-27
+# file: mpl_CFG.py
+# tested with python 2.7.15 in conjunction with mpl version 2.2.3
+# tested with python 3.7.0  in conjunction with mpl version 3.0.2
+##########################################################################################
+
+import os
+import datetime
+import numpy as np
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+from scipy.stats import norm
+
+def ensure_dir(dir):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+now = datetime.datetime.now()
+now = "%s-%s-%s" %(now.year, str(now.month).zfill(2), str(now.day).zfill(2))
+
+BASEDIR = os.path.dirname(os.path.abspath(__file__))
+RAWDIR = os.path.join(BASEDIR, 'raw')
+OUTDIR = os.path.join(BASEDIR, 'out')
+
+ensure_dir(OUTDIR)
+
+def getFigureProps(width, height, lFrac = 0.17, rFrac = 0.9, bFrac = 0.17, tFrac = 0.9):
+    '''
+    True size scaling auxiliary function to setup mpl plots with a desired size.
+    Specify widht and height in cm.
+    lFrac = left fraction   in [0, 1]
+    rFrac = right fraction  in [0, 1]
+    bFrac = bottom fraction in [0, 1]
+    tFrac = top fraction    in [0, 1]
+    returns:
+        fWidth = figure width
+        fHeight = figure height
+    These figure width and height values can then be used to create a figure instance 
+    of the desired size, such that the actual plotting canvas has the specified
+    target width and height, as provided by the input parameters of this function.
+    '''
+    axesWidth = width / 2.54    # convert to inches
+    axesHeight = height / 2.54  # convert to inches
+    fWidth = axesWidth / (rFrac - lFrac)
+    fHeight = axesHeight / (tFrac - bFrac)
+    return fWidth, fHeight, lFrac, rFrac, bFrac, tFrac
+
+# plot color dictionary
+pColors = {'green': '#00FF00', # neon green
+           'red':   '#FF0000', # standard red
+           'blue':  '#0000FF'} # standard blue
+
+if __name__ == '__main__':
+    
+    # Playground for scipy's normal distribution implementation
+    
+    ######################################################################################
+    # IMPORTANT: Scipy's norm.pdf() takes the standard deviation and
+    # not the variance as scale parameter. This is one of the most frequent pitfalls
+    # when using normal distributions.
+    ######################################################################################
+    
+    # create normal distribution with specified mean and variance (location and shape)
+    # pdf function signature
+    # scipy.stats.norm(x, loc, scale)
+    
+    mu = 0.0            # mean of the normal distribution $\mu$
+    var = 1.5 ** 2      # variance of the normal distribution $\sigma^2§
+    
+    # xLeft and xRight are the x coordinates $\mu - \sigma$ and $\mu + \sigma$.
+    # Pay attention that we use the standard deviation $\sigma$ here and not the
+    # variance $\sigma^2$.
+    xLeft = mu - np.sqrt(var)
+    xRight = mu + np.sqrt(var)
+    
+    yLeft = norm.pdf(xLeft, mu, np.sqrt(var))
+    yRight = norm.pdf(xRight, mu, np.sqrt(var))
+    
+    assert np.isclose(yLeft, yRight), "Error: yLeft == yRight assertion failed."
