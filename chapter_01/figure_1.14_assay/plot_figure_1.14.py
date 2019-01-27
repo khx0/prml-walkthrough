fixed variance std confusion

chapter_01/figure_1.14_assay/plot_figure_1.14.py
@@ -55,7 +55,7 @@ def getFigureProps(width, height, lFrac = 0.17, rFrac = 0.9, bFrac = 0.17, tFrac
     fHeight = axesHeight / (tFrac - bFrac)
     return fWidth, fHeight, lFrac, rFrac, bFrac, tFrac
 
-def Plot(titlestr, X, Xs, params, outname, outdir, pColors, 
+def Plot(titlestr, X, Xs, outname, outdir, pColors, 
          grid = False, drawLegend = True, xFormat = None, yFormat = None, 
          savePDF = True, savePNG = False, datestamp = True):
 
@@ -254,12 +254,18 @@ if __name__ == '__main__':
     # pdf function signature
     # scipy.stats.norm(x, loc, scale)
     
+    ######################################################################################
+    # IMPORTANT: Scipy's norm.pdf() takes the standard deviation and
+    # not the variance as scale parameter. This is one of the most frequent pitfalls
+    # when using normal distributions.
+    ######################################################################################
+    
     mu = 3.0    # mean of the normal distribution $\mu$
     var = 1.0   # variance of the normal distribution $\sigma^2§
     
     nVisPoints = 800
     xVals = np.linspace(0.0, 20.0, nVisPoints)
-    yVals = np.array([norm.pdf(x, loc = mu, scale = var) for x in xVals])
+    yVals = np.array([norm.pdf(x, loc = mu, scale = np.sqrt(var)) for x in xVals])
     
     X = np.zeros((nVisPoints, 2))
     X[:, 0] = xVals
@@ -273,8 +279,8 @@ if __name__ == '__main__':
     xLeft = mu - np.sqrt(var)
     xRight = mu + np.sqrt(var)
     
-    yLeft = norm.pdf(xLeft, mu, var)
-    yRight = norm.pdf(xRight, mu, var)
+    yLeft = norm.pdf(xLeft, mu, np.sqrt(var))
+    yRight = norm.pdf(xRight, mu, np.sqrt(var))
     
     assert np.isclose(yLeft, yRight), "Error: yLeft == yRight assertion failed."
     
@@ -300,7 +306,6 @@ if __name__ == '__main__':
     outname = Plot(titlestr = '',
                    X = X,
                    Xs = Xs,
-                   params = [mu, var], 
                    outname = outname,
                    outdir = OUTDIR, 
                    pColors = pColors, 