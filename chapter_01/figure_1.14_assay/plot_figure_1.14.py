195ce0000fb943703b11f9a73c70697209998cb5
Nikolas Schnellbächer <khx0@posteo.net>
2019-01-28 09:42:53 +0100

    updated pColors dict

--- chapter_01/figure_1.14_assay/plot_figure_1.14.py
+++ chapter_01/figure_1.14_assay/plot_figure_1.14.py
@@ -3,7 +3,7 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2019-01-27
+# date: 2019-01-28
 # file: plot_figure_1.14.py
 # tested with python 2.7.15 in conjunction with mpl version 2.2.3
 # tested with python 3.7.0  in conjunction with mpl version 3.0.2
@@ -304,10 +304,6 @@ if __name__ == '__main__':
                'green': '#00FF00',  # neon green
                'red': '#FF0000'}    # standard red
     
-#     pColors = ['#FF0000', # standard red
-#                '#00FF00', # neon green
-#                '#0000FF'] # standard blue
-
     outname = Plot(titlestr = '',
                    X = X,
                    Xs = Xs,
