8d6cdce91fd1ac9204050578f3c95bc6e47ac960
Nikolas Schnellbächer <khx0@posteo.net>
2019-02-06 23:05:24 +0100

    file i/o

--- chapter_01/figure_1.17_bayesian_polynomial_curve_fitting/bayesian_polynomial_curve_fitting.py
+++ chapter_01/figure_1.17_bayesian_polynomial_curve_fitting/bayesian_polynomial_curve_fitting.py
@@ -3,7 +3,7 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2019-01-04
+# date: 2019-02-07
 # file: bayesian_polynomial_curve_fitting.py
 # tested with python 2.7.15
 # tested with python 3.7.0
@@ -28,38 +28,14 @@ ensure_dir(RAWDIR)
 
 if __name__ == '__main__':
         
-    # PRML - Bishop - Chapter 1 Introduction - Curve Fitting
-    # figure 1.2 training data
-    
-    ######################################################################################
-    # noise settings
-    
-    # fix random number seed for reproducibility
-    seedValue = 523456789
-    seed = np.random.seed(seedValue)
-    
-    # numpy.random.normal() function signature:
-    # numpy.random.normal(loc = 0.0, scale = 1.0, size = None)
-    # loc = mean ($\mu$)
-    # scale = standard deviation ($\sigma$)
-    # $\mathcal{N}(\mu, \sigma^2)$
-    mu = 0.0
-    sigma = 0.3
-    
-    # number of training data points
-    # Xtrain = training data set
-    # create N training data points (N = 10)
-    N = 10
-    Xtrain = np.zeros((N, 2))
-    xtVals = np.linspace(0.0, 1.0, N)
-    ytVals = np.array([np.sin(2.0 * np.pi * x) + np.random.normal(mu, sigma) 
-                       for x in xtVals])
-    Xtrain[:, 0] = xtVals
-    Xtrain[:, 1] = ytVals
+    # PRML - Bishop - Chapter 1 Introduction - Bayesian Polynomial Curve Fitting
     
     ######################################################################################
     # file i/o
     
-    outname = 'prml_ch_01_figure_1.2_training_Data_PRNG-seed_{}.txt'.format(seedValue)
+    seedValue = 523456789
+    filename = 'prml_ch_01_figure_1.2_training_Data_PRNG-seed_{}.txt'.format(seedValue)
     
-    np.savetxt(os.path.join(RAWDIR, outname), Xtrain, fmt = '%.8f')
+    Xt = np.genfromtxt(os.path.join(RAWDIR, filename))
+
+    print("Xt.shape =", Xt.shape)
