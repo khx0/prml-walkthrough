e16673bc57bc243184cb7b076bc4c2d9fc3ca059
Nikolas Schnellbächer <khx0@posteo.net>
2019-02-07 08:54:55 +0100

    vectorization work

--- chapter_01/figure_1.17_bayesian_polynomial_curve_fitting/bayesian_polynomial_curve_fitting.py
+++ chapter_01/figure_1.17_bayesian_polynomial_curve_fitting/bayesian_polynomial_curve_fitting.py
@@ -27,7 +27,7 @@ OUTDIR = os.path.join(BASEDIR, 'out')
 ensure_dir(RAWDIR)
 
 if __name__ == '__main__':
-        
+    
     # PRML - Bishop - Chapter 1 Introduction - Bayesian Polynomial Curve Fitting
     
     ######################################################################################
@@ -36,6 +36,53 @@ if __name__ == '__main__':
     seedValue = 523456789
     filename = 'prml_ch_01_figure_1.2_training_Data_PRNG-seed_{}.txt'.format(seedValue)
     
-    Xt = np.genfromtxt(os.path.join(RAWDIR, filename))
+    X = np.genfromtxt(os.path.join(RAWDIR, filename))
+    assert X.shape[1] == 2, "Error: Shape assertion failed."
+    print("X.shape =", X.shape)
+    nDatapoints = X.shape[0]
+    
+    ######################################################################################
+    # set parameters for this problem
+    alpha = 5.0e-3
+    beta = 11.1
+    M = 9 # order of polynomial
+    D = M + 1 # dimensionality
+    
+    
+    # x query point
+    x = np.array([0.5])
+    
+    ########## FIX ME #############
+    # construct ... *** ...
+    
+    phiMat = np.ones((D, nDatapoints))
+    for n in range(nDatapoints):    
+        for i in range(1, D, 1):
+            phiMat[i, n] *= X[n, 0] ** i
+    
+    
+    a = np.matmul(phiMat, X[:, 1])
+    
+    
+    
+    
+    
+    print(tmp.shape)
+    print(X[:, 1].shape)
+    print(a.shape)
+    
+    
+    
+    
+    '''
+    # fill the Vandermonde matrix V
+    V = np.ones((nDatapoints, m + 1))
+    
 
-    print("Xt.shape =", Xt.shape)
+    
+    for i in range(m):
+        tmp = np.multiply(tmp, X[:, 0])
+        V[:, i + 1] = tmp
+    '''
+    
+    
