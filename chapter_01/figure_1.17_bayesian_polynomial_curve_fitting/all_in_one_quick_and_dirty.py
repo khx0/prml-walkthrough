ba8a08973bae1e6fb740b0c7a324c3bae99c390a
Nikolas Schnellbächer <khx0@posteo.net>
2019-02-08 23:07:28 +0100

    updated quick and dirty version

--- chapter_01/figure_1.17_bayesian_polynomial_curve_fitting/all_in_one_quick_and_dirty.py
+++ chapter_01/figure_1.17_bayesian_polynomial_curve_fitting/all_in_one_quick_and_dirty.py
@@ -5,49 +5,89 @@
 # contact: khx0@posteo.net
 # date: 2019-02-08
 # file: all_in_one_quick_and_dirty.py
-# tested with python 2.7.15
-# tested with python 3.7.0
+# tested with python 2.7.15 and matplotlib 2.2.3
+# tested with python 3.7.0 and matplotlib 3.0.2
 ##########################################################################################
+
+'''
+Quick and dirty version of the bayesian polynomial curve fitting example
+as illustrated in figure 1.17 of chapter 1 of Bishop's "Pattern Recognition and
+Machine Learning" book. Not meant for easy accessibility due to its brevity
+but rather as show case.
+'''
 
 import os
 import datetime
 import numpy as np
-
-def ensure_dir(dir):
-    if not os.path.exists(dir):
-        os.makedirs(dir)
-
-now = datetime.datetime.now()
-now = "{}-{}-{}".format(now.year, str(now.month).zfill(2), str(now.day).zfill(2))
-
-BASEDIR = os.path.dirname(os.path.abspath(__file__))
-RAWDIR = os.path.join(BASEDIR, 'raw')
-OUTDIR = os.path.join(BASEDIR, 'out')
-
-ensure_dir(RAWDIR)
+import matplotlib as mpl
+from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
     
-    pass
+    np.random.seed(523456789)
     
-    '''
-    data = np.genfromtxt(os.path.join(RAWDIR, filename))
-    nDatapoints = data.shape[0]
-    X, T = data[:, 0], data[:, 1] # using the Bishop naming convention
+    N = 10 # number of training data points
+    X = np.linspace(0.0, 1.0, N)
+    T = np.sin(2.0 * np.pi * X) + np.random.normal(0.0, 0.3, N)
+    X_gt = np.linspace(0.0, 1.0, 301)
+    T_gt = np.sin(2.0 * np.pi * X_gt)
     
-    ######################################################################################
-    # set (hyper-) parameters for this problem
+    # set parameters
     alpha = 5.0e-3
     beta = 11.1
-    M = 9 # order of polynomial
-    xSupport = np.linspace(0.0, 1.0, 301)
+    M = 9       # order of polynomial
+    D = M + 1   # dimension
     
-    res = bayesianPolyCurveFit(xSupport, X, T, alpha, beta, M)
+    # perform bayesian polynomial curve fitting
+    V = np.ones((D, N)) 
+    for n in range(N):
+        V[:, n] = np.power(X[n], np.arange(0, D, 1))
+    
+    rhs = np.matmul(V, T)
+    Sinv = alpha * np.eye(D) + beta * np.matmul(V, V.T)
+    xData = np.linalg.solve(Sinv, rhs)
+    mean = np.zeros((len(X_gt),))
+    var = np.zeros((len(X_gt),))
+    for i in range(len(mean)):    
+        px = np.power(X_gt[i], np.arange(0, D, 1))
+        xPrediction = np.linalg.solve(Sinv, px)
+        mean[i] = (px.T).dot(xData)
+        var[i] = (px.T).dot(xPrediction)
+    
+    mean *= beta
+    var += 1.0 / beta
+    
+    # plot result
+    f, ax1 = plt.subplots(1)
+    ax1.plot(X_gt, T_gt, color = '#00FF00', zorder = 2)
+    ax1.scatter(X, T, facecolor = 'None',
+                      edgecolor = '#0000FF',
+                      zorder = 4)
+    ax1.plot(X_gt, mean, color = '#FF0000', zorder = 3)
+    ax1.fill_between(X_gt, mean - np.sqrt(var), mean + np.sqrt(var), 
+             color = '#FF0000',
+             alpha = 0.20,
+             lw = 0.0,
+             zorder = 1)
+        
+    ax1.set_xlim(-0.05, 1.05)    
+    ax1.set_xlabel(r'$x$')
+    ax1.set_ylabel(r'$t$')        
+    f.savefig('figure_1.17_minimal_quick_and_dirty.pdf',
+              dpi = 300,
+              transparent = True)
+    
+    plt.show()
     
     ######################################################################################
-    # output file i/o
-    
-    outname = 'prml_ch_01_figure_1.17_bayesianPolyCurveFit_M_{}.txt'.format(M)
-    
-    np.savetxt(os.path.join(RAWDIR, outname), res, fmt = '%.8f')
-    '''
+    import platform
+    print(platform.python_version())
+    print(mpl.__version__)
+    ######################################################################################
+    outname = 'TEST.txt'
+    res = np.zeros((len(X_gt), 3))
+    res[:, 0] = X_gt
+    res[:, 1] = mean
+    res[:, 2] = var
+    np.savetxt(outname, res, fmt = '%.8f')
+    ######################################################################################
