be305445907bc5bf7e98b88be7e1dd6dff9ce7a8
Nikolas Schnellbächer <khx0@posteo.net>
2019-02-09 10:45:43 +0100

    added 1st ffmpeg script

---
+++ chapter_01/figure_1.17_bayesian_polynomial_curve_fitting/make_movie.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+##########################################################################################
+# make_movie shell script
+# author: Nikolas Schnellbaecher
+# contact: khx0@posteo.net
+# file: make_movie.sh
+# date: 2019-02-09
+##########################################################################################
+
+#ffmpeg -framerate 8 -i ./frames/frame_wHist_%03d.png -r 30 -f mp4 -vcodec libx264 -pix_fmt yuv420p -vb 20M striped_major.mp4
+ffmpeg -framerate 4 -pattern_type glob -i './out/increment_N/*.png' -r 30 -f mp4 -vcodec libx264 -pix_fmt yuv420p test.mp4
+
+# -vb 20M
