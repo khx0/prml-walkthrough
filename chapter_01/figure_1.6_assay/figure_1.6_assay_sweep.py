b0779727ad64b4b47708284a1b9fdddcc5926f3d
Nikolas Schnellbächer <khx0@posteo.net>
2019-03-18 22:08:25 +0100

    fixed consistency issues

--- chapter_01/figure_1.6_assay/figure_1.6_assay_sweep.py
+++ chapter_01/figure_1.6_assay/figure_1.6_assay_sweep.py
@@ -52,7 +52,7 @@ def getFigureProps(width, height, lFrac = 0.17, rFrac = 0.9, bFrac = 0.17, tFrac
     returns:
         fWidth = figure width
         fHeight = figure height
-    These figure width and height values can then be used to create a figure instance 
+    These figure width and height values can then be used to create a figure instance
     of the desired size, such that the actual plotting canvas has the specified
     target width and height, as provided by the input parameters of this function.
     '''
@@ -62,49 +62,49 @@ def getFigureProps(width, height, lFrac = 0.17, rFrac = 0.9, bFrac = 0.17, tFrac
     fHeight = axesHeight / (tFrac - bFrac)
     return fWidth, fHeight, lFrac, rFrac, bFrac, tFrac
 
-def Plot(titlestr, X, Xt, Xm, params, zorders, outname, outdir, pColors, 
-         grid = False, drawLegend = True, xFormat = None, yFormat = None, 
+def Plot(titlestr, X, Xt, Xm, params, zorders, outname, outdir, pColors,
+         grid = False, drawLegend = True, xFormat = None, yFormat = None,
          savePDF = True, savePNG = False, datestamp = True):
-    
+
     mpl.rcParams['xtick.top'] = True
     mpl.rcParams['xtick.bottom'] = True
     mpl.rcParams['ytick.right'] = True
     mpl.rcParams['xtick.direction'] = 'in'
     mpl.rcParams['ytick.direction'] = 'in'
-    
+
     mpl.rc('font', **{'size': 10})
     mpl.rc('legend', **{'fontsize': 7.0})
-    mpl.rc("axes", linewidth = 0.5)    
-    
+    mpl.rc("axes", linewidth = 0.5)
+
     # plt.rc('font', **{'family' : 'sans-serif', 'sans-serif' : ['Myriad Pro']})
     plt.rc('font', **{'family' : 'sans-serif', 'sans-serif' : ['Helvetica']})
-    plt.rcParams['pdf.fonttype'] = 42  
+    plt.rcParams['pdf.fonttype'] = 42
     mpl.rcParams['text.usetex'] = False
     mpl.rcParams['mathtext.fontset'] = 'cm'
     fontparams = {'text.latex.preamble': [r'\usepackage{cmbright}',
                                           r'\usepackage{amsmath}']}
-    mpl.rcParams.update(fontparams)     
-    
+    mpl.rcParams.update(fontparams)
+
     ######################################################################################
     # set up figure
     fWidth, fHeight, lFrac, rFrac, bFrac, tFrac =\
         getFigureProps(width = 4.1, height = 2.9,
                        lFrac = 0.10, rFrac = 0.95, bFrac = 0.15, tFrac = 0.95)
     f, ax1 = plt.subplots(1)
-    f.set_size_inches(fWidth, fHeight)    
+    f.set_size_inches(fWidth, fHeight)
     f.subplots_adjust(left = lFrac, right = rFrac)
     f.subplots_adjust(bottom = bFrac, top = tFrac)
     ######################################################################################
     labelfontsize = 6.0
-    
+
     for tick in ax1.xaxis.get_major_ticks():
         tick.label.set_fontsize(labelfontsize)
     for tick in ax1.yaxis.get_major_ticks():
         tick.label.set_fontsize(labelfontsize)
-    
+
     ax1.tick_params('both', length = 1.5, width = 0.5, which = 'major', pad = 3.0)
     ax1.tick_params('both', length = 1.0, width = 0.25, which = 'minor', pad = 3.0)
-    
+
     ax1.tick_params(axis = 'x', which = 'major', pad = 2.0)
     ax1.tick_params(axis = 'y', which = 'major', pad = 2.0, zorder = 10)
     ######################################################################################
@@ -114,19 +114,19 @@ def Plot(titlestr, X, Xt, Xm, params, zorders, outname, outdir, pColors,
     # rotation is expressed in degrees
     ax1.set_ylabel(r'$t$', fontsize = 6.0, y = 0.70, rotation = 0.0)
     ax1.xaxis.labelpad = -1.75
-    ax1.yaxis.labelpad = -1.75 
+    ax1.yaxis.labelpad = -1.75
     ######################################################################################
     # plotting
-    
-    lineWidth = 0.65    
-    
-    ax1.plot(X[:, 0], X[:, 1], 
+
+    lineWidth = 0.65
+
+    ax1.plot(X[:, 0], X[:, 1],
              color = pColors[0],
              alpha = 1.0,
              lw = lineWidth,
              zorder = zorders[0],
              label = r'')
-    
+
     ax1.scatter(Xt[:, 0], Xt[:, 1],
                 s = 10.0,
                 lw = lineWidth,
@@ -134,49 +134,49 @@ def Plot(titlestr, X, Xt, Xm, params, zorders, outname, outdir, pColors,
                 edgecolor = pColors[1],
                 zorder = zorders[1],
                 label = r'')
-    
-    ax1.plot(Xm[:, 0], Xm[:, 1], 
+
+    ax1.plot(Xm[:, 0], Xm[:, 1],
              color = pColors[2],
              alpha = 1.0,
              lw = lineWidth,
              zorder = zorders[2],
              label = r'')
-    
+
     ######################################################################################
     # annotations
-    
+
     label = r'$M = 9$'
-    
+
     x_pos = 0.80
-    
+
     ax1.annotate(label,
                  xy = (x_pos, 0.89),
                  xycoords = 'axes fraction',
-                 fontsize = 5.0, 
+                 fontsize = 5.0,
                  horizontalalignment = 'left')
-    
+
     label = r'$N = %d$' %(params[0])
-    
+
     ax1.annotate(label,
                  xy = (x_pos, 0.79),
                  xycoords = 'axes fraction',
-                 fontsize = 5.0, 
+                 fontsize = 5.0,
                  horizontalalignment = 'left')
-    
+
     ######################################################################################
     # legend
     if drawLegend:
         leg = ax1.legend(# bbox_to_anchor = [0.7, 0.8],
                          # loc = 'upper left',
-                         handlelength = 1.5, 
+                         handlelength = 1.5,
                          scatterpoints = 1,
                          markerscale = 1.0,
                          ncol = 1)
         leg.draw_frame(False)
         plt.gca().add_artist(leg)
-    
+
     ######################################################################################
-    # set plot range  
+    # set plot range
     if (xFormat == None):
         pass
     else:
@@ -185,7 +185,7 @@ def Plot(titlestr, X, Xt, Xm, params, zorders, outname, outdir, pColors,
         ax1.set_xticks(major_x_ticks)
         ax1.set_xticks(minor_x_ticks, minor = True)
         ax1.set_xlim(xFormat[0], xFormat[1])
-    
+
     if (yFormat == None):
         pass
     else:
@@ -194,12 +194,12 @@ def Plot(titlestr, X, Xt, Xm, params, zorders, outname, outdir, pColors,
         ax1.set_yticks(major_y_ticks)
         ax1.set_yticks(minor_y_ticks, minor = True)
         ax1.set_ylim(yFormat[0], yFormat[1])
-    
+
     ax1.set_axisbelow(False)
-    
+
     for spine in ax1.spines.values():  # ax1.spines is a dictionary
         spine.set_zorder(10)
-    
+
     ######################################################################################
     # grid options
     if grid:
@@ -232,74 +232,75 @@ def polynomial_horner(x, *coeff):
     return res
 
 if __name__ == '__main__':
-    
+
     # PRML Bishop chapter 1 Introduction - Curve Fitting - figure 1.6 assay (Ntrain sweep)
-    
+
     # global parameters
     mu = 0.0
     sigma = 0.3
-    
+
     m = 9 					# polynomial fitting degree
     w = np.ones((m + 1,))	# initial weight vector
-    
+
     nVisPoints = 800
     nModelPoints = 800
-    
+
     xVals = np.linspace(0.0, 1.0, nVisPoints)
     yVals = np.sin(2.0 * np.pi * xVals)
     # X = ground truth
     X = np.zeros((nVisPoints, 2))
     X[:, 0] = xVals
     X[:, 1] = yVals
-    
+
     nTrainVals = [15, 100, 200, 300, 500]
-    seeds = [323456789, 923456789, 923456789, 923456789, 923456789]
     
+    seeds = [323456789, 923456789, 923456789, 923456789, 923456789]
+
     pColors = ['#00FF00', # neon green
                '#0000FF', # standard blue
                '#FF0000'] # standard red
-    
+
     xFormatList = [[-0.05, 1.05, 0.0, 1.1, 1.0, 1.0],
                    [-0.05, 1.05, 0.0, 1.1, 1.0, 1.0],
                    [-0.05, 1.05, 0.0, 1.1, 1.0, 1.0],
                    [-0.05, 1.05, 0.0, 1.1, 1.0, 1.0],
                    [-0.05, 1.05, 0.0, 1.1, 1.0, 1.0]]
-    
+
     yFormatList = [[-1.5, 1.5, -1.0, 1.1, 1.0, 1.0],
                    [-1.5, 1.5, -1.0, 1.1, 1.0, 1.0],
                    [-1.7, 1.7, -1.0, 1.1, 1.0, 1.0],
                    [-1.7, 1.7, -1.0, 1.1, 1.0, 1.0],
                    [-1.7, 1.7, -1.0, 1.1, 1.0, 1.0]]
-                   
+
     zorders = [[2, 3, 2],
                [2, 3, 4],
                [2, 1, 2],
                [2, 1, 2],
                [2, 1, 2]]
-    
+
     for i, nTrain in enumerate(nTrainVals):
-    	
+
         # fix random number seed for reproducibility
         np.random.seed(seeds[i])
-        
+
         # create training data
         xVals = np.linspace(0.0, 1.0, nTrain)
         yVals = np.sin(2.0 * np.pi * xVals) + np.random.normal(mu, sigma, xVals.shape)
         Xt = np.zeros((nTrain, 2))
         Xt[:, 0] = xVals
         Xt[:, 1] = yVals
-        
+
         ##################################################################################
         # file i/o
         outname = 'figure_1.6_training_data_N_%d_PRNG-seed_%d.txt' %(nTrain, seeds[i])
         np.savetxt(os.path.join(RAWDIR, outname), Xt, fmt = '%.8f')
         ##################################################################################
-     
+
         ##################################################################################
         # polynomial curve fitting (learning the model)
-        
+
         popt, pcov = curve_fit(polynomial_horner, Xt[:, 0], Xt[:, 1], p0 = w)
-    	
+
         # create fitted model
         xVals = np.linspace(0.0, 1.0, nModelPoints)
         yVals = np.zeros_like(xVals)
@@ -307,16 +308,16 @@ if __name__ == '__main__':
         Xm = np.zeros((nModelPoints, 2))
         Xm[:, 0] = xVals
         Xm[:, 1] = yVals
-        
+
         ##################################################################################
         # file i/o
         outname = 'figure_1.6_fitted_model_N_%d_PRNG-seed_%d.txt' %(nTrain, seeds[i])
         np.savetxt(os.path.join(RAWDIR, outname), X, fmt = '%.8f')
         ##################################################################################
-        
-        # call the plotting function     
+
+        # call the plotting function
         outname = 'figure_1.6_N_%d_PRNG-seed_%d' %(nTrain, seeds[i])
-                
+
         outname = Plot(titlestr = '',
                        X = X,
                        Xt = Xt,
@@ -324,9 +325,9 @@ if __name__ == '__main__':
                        params = [nTrain],
                        zorders = zorders[i],
                        outname = outname,
-                       outdir = OUTDIR, 
-                       pColors = pColors, 
-                       grid = False, 
-                       drawLegend = False, 
+                       outdir = OUTDIR,
+                       pColors = pColors,
+                       grid = False,
+                       drawLegend = False,
                        xFormat = xFormatList[i],
                        yFormat = yFormatList[i])
