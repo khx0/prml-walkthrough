e08b01840dcbae55d7ec20ba60f2858f1586a439
Nikolas Schnellbächer <khx0@posteo.net>
2019-03-21 23:20:04 +0100

    updated figure 1.5 assay

--- chapter_01/polynomial_least_squares_curve_fitting/figure_1.5_assay/figure_1.5_assay.py
+++ chapter_01/polynomial_least_squares_curve_fitting/figure_1.5_assay/figure_1.5_assay.py
@@ -3,23 +3,24 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2018-10-02
+# date: 2019-03-21
 # file: figure_1.5_assay
 # tested with python 2.7.15 in conjunction with mpl version 2.2.3
-# tested with python 3.7.0  in conjunction with mpl version 2.2.3
+# tested with python 3.7.2  in conjunction with mpl version 3.0.3
 ##########################################################################################
 
 import sys
 sys.path.append('../')
-import time
-import datetime
+sys.path.append('../../../lib')
 import os
-import math
+import datetime
 import numpy as np
 import matplotlib as mpl
 from matplotlib import pyplot as plt
 from matplotlib import rc
 from matplotlib.pyplot import legend
+
+from polynomials import polynomial_horner
 
 from polyLeastSquares import polyLeastSquares
 
@@ -58,10 +59,10 @@ def getFigureProps(width, height, lFrac = 0.17, rFrac = 0.9, bFrac = 0.17, tFrac
     fHeight = axesHeight / (tFrac - bFrac)
     return fWidth, fHeight, lFrac, rFrac, bFrac, tFrac
 
-def Plot(titlestr, X, params, outname, outdir, pColors, 
-        grid = False, drawLegend = True, xFormat = None, yFormat = None, 
-        savePDF = True, savePNG = False, datestamp = True):
-
+def Plot(titlestr, X, outname, outdir, pColors, 
+         grid = False, drawLegend = True, xFormat = None, yFormat = None, 
+         savePDF = True, savePNG = False, datestamp = True):
+    
     mpl.rcParams['xtick.top'] = True
     mpl.rcParams['xtick.bottom'] = True
     mpl.rcParams['ytick.right'] = True
@@ -91,19 +92,17 @@ def Plot(titlestr, X, params, outname, outdir, pColors,
     f.subplots_adjust(bottom = bFrac, top = tFrac)
     ######################################################################################
     labelfontsize = 6.0
-
+    
     for tick in ax1.xaxis.get_major_ticks():
         tick.label.set_fontsize(labelfontsize)
     for tick in ax1.yaxis.get_major_ticks():
         tick.label.set_fontsize(labelfontsize)
-        
-    xticks = plt.getp(plt.gca(), 'xticklines')
-    yticks = plt.getp(plt.gca(), 'yticklines')
+    
     ax1.tick_params('both', length = 1.5, width = 0.5, which = 'major', pad = 3.0)
     ax1.tick_params('both', length = 1.0, width = 0.25, which = 'minor', pad = 3.0)
-
-    ax1.tick_params(axis='x', which='major', pad = 3.5)
-    ax1.tick_params(axis='y', which='major', pad = 3.5, zorder = 10)
+    
+    ax1.tick_params(axis = 'x', which = 'major', pad = 3.5)
+    ax1.tick_params(axis = 'y', which = 'major', pad = 3.5, zorder = 10)
     ######################################################################################
     # labeling
     plt.title(titlestr)
@@ -113,9 +112,9 @@ def Plot(titlestr, X, params, outname, outdir, pColors,
     ax1.yaxis.labelpad = 3.0 
     ######################################################################################
     # plotting
-        
+    
     lineWidth = 0.65    
-
+    
     # plot test error
     ax1.plot(X[:, 0], X[:, 2], 
              color = pColors[1],
@@ -142,7 +141,7 @@ def Plot(titlestr, X, params, outname, outdir, pColors,
              zorder = 11,
              label = r'',
              clip_on = False)
-             
+    
     ax1.scatter(X[:, 0], X[:, 1],
                 s = 10.0,
                 lw = lineWidth,
@@ -151,14 +150,12 @@ def Plot(titlestr, X, params, outname, outdir, pColors,
                 zorder = 11,
                 label = r'Training',
                 clip_on = False)
-                
-
-             
+    
     ######################################################################################
     # legend
-    if (drawLegend):
-        leg = ax1.legend(#bbox_to_anchor = [0.7, 0.8],
-                         #loc = 'upper left',
+    if drawLegend:
+        leg = ax1.legend(# bbox_to_anchor = [0.7, 0.8],
+                         # loc = 'upper left',
                          handlelength = 1.5, 
                          scatterpoints = 1,
                          markerscale = 1.0,
@@ -176,7 +173,7 @@ def Plot(titlestr, X, params, outname, outdir, pColors,
         ax1.set_xticks(major_x_ticks)
         ax1.set_xticks(minor_x_ticks, minor = True)
         ax1.set_xlim(xFormat[0], xFormat[1])
-        
+    
     if (yFormat == None):
         pass
     else:
@@ -191,25 +188,26 @@ def Plot(titlestr, X, params, outname, outdir, pColors,
         print("ATTENTION: MANUAL Y TICKS USED.")
         ax1.set_yticklabels([0, 0.5, 1])
         ###########################################
-        
+    
     ax1.set_axisbelow(False)
-    for k, spine in ax1.spines.items():  #ax.spines is a dictionary
+    
+    for spine in ax1.spines.values():  # ax1.spines is a dictionary
         spine.set_zorder(10)
     
     ######################################################################################
     # grid options
-    if (grid):
+    if grid:
         ax1.grid(color = 'gray', linestyle = '-', alpha = 0.2, which = 'major', linewidth = 0.2)
         ax1.grid('on')
         ax1.grid(color = 'gray', linestyle = '-', alpha = 0.05, which = 'minor', linewidth = 0.1)
         ax1.grid('on', which = 'minor')
     ######################################################################################
     # save to file
-    if (datestamp):
+    if datestamp:
         outname += '_' + now
-    if (savePDF):
+    if savePDF:
         f.savefig(os.path.join(outdir, outname) + '.pdf', dpi = 300, transparent = True)
-    if (savePNG):
+    if savePNG:
         f.savefig(os.path.join(outdir, outname) + '.png', dpi = 600, transparent = False)
     ######################################################################################
     # close handles
@@ -217,18 +215,6 @@ def Plot(titlestr, X, params, outname, outdir, pColors,
     plt.clf()
     plt.close()
     return outname
-
-def poly_horner(x, *coeff):
-    result = coeff[-1]
-    for i in range(-2, -len(coeff)-1, -1):
-        result = result*x + coeff[i]
-    return result
-
-def poly_horner2(x, coeff):
-    result = coeff[-1]
-    for i in range(-2, -len(coeff)-1, -1):
-        result = result*x + coeff[i]
-    return result
 
 if __name__ == '__main__':
     
@@ -238,7 +224,7 @@ if __name__ == '__main__':
     # global parameters
     nTrain = 10
     nTest = 100
-
+    
     # noise settings
     # numpy.random.normal() function signature:
     # numpy.random.normal(loc = 0.0, scale = 1.0, size = None)
@@ -257,17 +243,14 @@ if __name__ == '__main__':
     # create training data
     Xt = np.zeros((nTrain, 2))
     xVals = np.linspace(0.0, 1.0, nTrain)
-    yVals = np.array([np.sin(2.0 * np.pi * x) + np.random.normal(mu, sigma) 
-                      for x in xVals])
+    yVals = np.sin(2.0 * np.pi * xVals) + np.random.normal(mu, sigma, xVals.shape)
     Xt[:, 0] = xVals
     Xt[:, 1] = yVals
     
     ######################################################################################
     # create test data
     xVals = np.linspace(0.0, 1.0, nTest)
-    yVals = np.array([np.sin(2.0 * np.pi * x) + np.random.normal(mu, sigma) 
-                          for x in xVals])
-    
+    yVals = np.sin(2.0 * np.pi * xVals) + np.random.normal(mu, sigma, xVals.shape)    
     X = np.zeros((nTest, 2))
     X[:, 0] = xVals
     X[:, 1] = yVals
@@ -285,14 +268,14 @@ if __name__ == '__main__':
         
         # polynomial least square curve fitting
         w = polyLeastSquares(m, Xt)
-                
-        yPredict = np.array([poly_horner2(x, w) for x in Xt[:, 0]])
+        
+        yPredict = polynomial_horner(Xt[:, 0], *w)
         
         # test data set prediction
-        yPredictTest = np.array([poly_horner2(x, w) for x in X[:, 0]])
+        yPredictTest = polynomial_horner(X[:, 0], *w)
         
         # compute sum of squares deviation
-                
+        
         sum_of_squares_error = 0.5 * np.sum(np.square(yPredict - Xt[:, 1]))
         sum_of_squares_error_test = 0.5 * np.sum(np.square(yPredictTest - X[:, 1]))
         
@@ -305,9 +288,7 @@ if __name__ == '__main__':
     
     ######################################################################################
     # file i/o
-    
     outname = 'figure_1.5_data_PRNG-seed_%d.txt' %(seedValue)
-    
     np.savetxt(os.path.join(RAWDIR, outname), res, fmt = '%.8f')
     ######################################################################################
     
@@ -324,7 +305,6 @@ if __name__ == '__main__':
     
     outname = Plot(titlestr = '',
                    X = res,
-                   params = [], 
                    outname = outname,
                    outdir = OUTDIR, 
                    pColors = pColors, 
@@ -332,7 +312,3 @@ if __name__ == '__main__':
                    drawLegend = True, 
                    xFormat = xFormat,
                    yFormat = yFormat)
-
-    
-    
-    
