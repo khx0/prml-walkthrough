3a3bd3cbfd4b29566cc4927f73247b533c443669
Nikolas Schnellbächer <khx0@posteo.net>
2019-03-25 20:44:57 +0100

    updated figure 1.14 scripts

--- chapter_01/figure_1.14_assay/plot_figure_1.14.py
+++ chapter_01/figure_1.14_assay/plot_figure_1.14.py
@@ -3,7 +3,7 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2019-03-10
+# date: 2019-03-25
 # file: plot_figure_1.14.py
 # tested with python 2.7.15 in conjunction with mpl version 2.2.3
 # tested with python 3.7.2  in conjunction with mpl version 3.0.3
@@ -14,25 +14,20 @@ import datetime
 import numpy as np
 import matplotlib as mpl
 from matplotlib import pyplot as plt
-from matplotlib import rc
 from matplotlib.pyplot import legend
 
 from scipy.stats import norm
 
 mpl.ticker._mathdefault = lambda x: '\\mathdefault{%s}'%x
 
-def ensure_dir(dir):
-    if not os.path.exists(dir):
-        os.makedirs(dir)
-
 now = datetime.datetime.now()
-now = "%s-%s-%s" %(now.year, str(now.month).zfill(2), str(now.day).zfill(2))
+now = "{}-{}-{}".format(now.year, str(now.month).zfill(2), str(now.day).zfill(2))
 
 BASEDIR = os.path.dirname(os.path.abspath(__file__))
 RAWDIR = os.path.join(BASEDIR, 'raw')
 OUTDIR = os.path.join(BASEDIR, 'out')
 
-ensure_dir(OUTDIR)
+os.makedirs(OUTDIR, exist_ok = True)
 
 def getFigureProps(width, height, lFrac = 0.17, rFrac = 0.9, bFrac = 0.17, tFrac = 0.9):
     '''
@@ -45,7 +40,7 @@ def getFigureProps(width, height, lFrac = 0.17, rFrac = 0.9, bFrac = 0.17, tFrac
     returns:
         fWidth = figure width
         fHeight = figure height
-    These figure width and height values can then be used to create a figure instance 
+    These figure width and height values can then be used to create a figure instance
     of the desired size, such that the actual plotting canvas has the specified
     target width and height, as provided by the input parameters of this function.
     '''
@@ -55,10 +50,10 @@ def getFigureProps(width, height, lFrac = 0.17, rFrac = 0.9, bFrac = 0.17, tFrac
     fHeight = axesHeight / (tFrac - bFrac)
     return fWidth, fHeight, lFrac, rFrac, bFrac, tFrac
 
-def Plot(titlestr, X, Xs, outname, outdir, pColors, 
-         grid = False, drawLegend = True, xFormat = None, yFormat = None, 
+def Plot(titlestr, X, Xs, outname, outdir, pColors,
+         grid = False, drawLegend = True, xFormat = None, yFormat = None,
          savePDF = True, savePNG = False, datestamp = True):
-
+    
     mpl.rcParams['xtick.top'] = False
     mpl.rcParams['xtick.bottom'] = True
     mpl.rcParams['ytick.right'] = False
@@ -71,12 +66,12 @@ def Plot(titlestr, X, Xs, outname, outdir, pColors,
     
     # mpl.rc('font', **{'family' : 'sans-serif', 'sans-serif' : ['Myriad Pro']})
     mpl.rc('font', **{'family' : 'sans-serif', 'sans-serif' : ['Helvetica']})
-    mpl.rcParams['pdf.fonttype'] = 42  
+    mpl.rcParams['pdf.fonttype'] = 42
     mpl.rcParams['text.usetex'] = False
     mpl.rcParams['mathtext.fontset'] = 'cm'
     fontparams = {'text.latex.preamble': [r'\usepackage{cmbright}',
                                           r'\usepackage{amsmath}']}
-    mpl.rcParams.update(fontparams)     
+    mpl.rcParams.update(fontparams)
     
     ######################################################################################
     # set up figure
@@ -85,14 +80,14 @@ def Plot(titlestr, X, Xs, outname, outdir, pColors,
                        lFrac = 0.15, rFrac = 0.95,
                        bFrac = 0.15, tFrac = 0.95)
     f, ax1 = plt.subplots(1)
-    f.set_size_inches(fWidth, fHeight)    
+    f.set_size_inches(fWidth, fHeight)
     f.subplots_adjust(left = lFrac, right = rFrac)
     f.subplots_adjust(bottom = bFrac, top = tFrac)
     
     # minimal layout
     ax1.spines['right'].set_visible(False)
     
-    ax1.spines['top'].set_visible(False)    
+    ax1.spines['top'].set_visible(False)
     
     ######################################################################################
     labelfontsize = 6.0
@@ -101,10 +96,10 @@ def Plot(titlestr, X, Xs, outname, outdir, pColors,
         tick.label.set_fontsize(labelfontsize)
     for tick in ax1.yaxis.get_major_ticks():
         tick.label.set_fontsize(labelfontsize)
-        
+    
     ax1.tick_params('both', length = 4.0, width = 0.5, which = 'major', pad = 3.0)
     ax1.tick_params('both', length = 1.0, width = 0.25, which = 'minor', pad = 3.0)
-
+    
     ax1.tick_params(axis = 'x', which = 'major', pad = 2.0)
     ax1.tick_params(axis = 'y', which = 'major', pad = 2.0, zorder = 10)
     ######################################################################################
@@ -119,22 +114,22 @@ def Plot(titlestr, X, Xs, outname, outdir, pColors,
     ######################################################################################
     # plotting
     
-    lineWidth = 1.0  
+    lineWidth = 1.0
     
-    ax1.plot(X[:, 0], X[:, 1], 
+    ax1.plot(X[:, 0], X[:, 1],
              color = pColors['red'],
              alpha = 1.0,
              lw = lineWidth,
              zorder = 2,
              label = r'')
-             
+    
     ax1.scatter(Xs[:, 0], Xs[:, 1],
                 s = 10.0,
                 lw = lineWidth,
                 facecolor = pColors['blue'],
                 edgecolor = 'None',
                 zorder = 3)
-                
+    
     ax1.scatter(Xs[:, 0], len(Xs) * [0.0],
                 s = 10.0,
                 lw = lineWidth,
@@ -142,7 +137,7 @@ def Plot(titlestr, X, Xs, outname, outdir, pColors,
                 edgecolor = 'None',
                 zorder = 3,
                 clip_on = False)
-                
+    
     for i in range(len(Xs)):
         
         ax1.plot([Xs[i, 0], Xs[i, 0]], [0.0, Xs[i, 1]],
@@ -179,21 +174,21 @@ def Plot(titlestr, X, Xs, outname, outdir, pColors,
     ax1.annotate(label,
                  xy = (x_pos, 0.50),
                  xycoords = 'axes fraction',
-                 fontsize = 8.0, 
+                 fontsize = 8.0,
                  horizontalalignment = 'center')
-                 
+    
     ax1.annotate(r'$x_n$',
                  xy = (0.53, -0.1),
                  xycoords = 'axes fraction',
-                 fontsize = 8.0, 
+                 fontsize = 8.0,
                  horizontalalignment = 'center')
-
+    
     ######################################################################################
     # legend
     if drawLegend:
         leg = ax1.legend(# bbox_to_anchor = [0.7, 0.8],
                          # loc = 'upper left',
-                         handlelength = 1.5, 
+                         handlelength = 1.5,
                          scatterpoints = 1,
                          markerscale = 1.0,
                          ncol = 1)
@@ -201,33 +196,33 @@ def Plot(titlestr, X, Xs, outname, outdir, pColors,
         plt.gca().add_artist(leg)
     
     ######################################################################################
-    # set plot range  
+    # set plot range
     if (xFormat == None):
         pass
     else:
         ax1.set_xlim(xFormat[0], xFormat[1])
         ax1.set_xticks([])
         ax1.set_xticklabels([])
-
+    
     if (yFormat == None):
         pass
     else:
         ax1.set_ylim(yFormat[0], yFormat[1])
         ax1.set_yticklabels([])
         ax1.set_yticks([])
-          
+    
     ax1.set_axisbelow(False)
-
+    
     for spine in ax1.spines.values(): # ax1.spines is a dictionary
         spine.set_zorder(10)
     
     ######################################################################################
     # grid options
     if grid:
-        ax1.grid(color = 'gray', linestyle = '-', alpha = 0.2, which = 'major', 
+        ax1.grid(color = 'gray', linestyle = '-', alpha = 0.2, which = 'major',
                  linewidth = 0.2)
         ax1.grid('on')
-        ax1.grid(color = 'gray', linestyle = '-', alpha = 0.05, which = 'minor', 
+        ax1.grid(color = 'gray', linestyle = '-', alpha = 0.05, which = 'minor',
                  linewidth = 0.1)
         ax1.grid('on', which = 'minor')
     ######################################################################################
@@ -244,7 +239,7 @@ def Plot(titlestr, X, Xs, outname, outdir, pColors,
     plt.clf()
     plt.close()
     return outname
-             
+
 if __name__ == '__main__':
     
     # figure 1.14 Bishop - Chapter 1 Introduction
@@ -308,9 +303,9 @@ if __name__ == '__main__':
                    X = X,
                    Xs = Xs,
                    outname = outname,
-                   outdir = OUTDIR, 
-                   pColors = pColors, 
-                   grid = False, 
-                   drawLegend = False, 
+                   outdir = OUTDIR,
+                   pColors = pColors,
+                   grid = False,
+                   drawLegend = False,
                    xFormat = xFormat,
                    yFormat = yFormat)
