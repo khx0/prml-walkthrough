27ff315c8a3cda004ca5170405c25d171ff4857e
Nikolas Schnellbächer <khx0@posteo.net>
2019-03-27 08:26:10 +0100

    clean up

--- chapter_01/polynomial_least_squares_curve_fitting/figure_1.7_assay/make_movie.sh
+++ chapter_01/polynomial_least_squares_curve_fitting/figure_1.7_assay/make_movie.sh
@@ -3,9 +3,9 @@
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
 # file: make_movie.sh
-# date: 2019-03-25
+# date: 2019-03-27
 ##########################################################################################
 
-ffmpeg -framerate 16 -pattern_type glob -i './frames/*.png' \
+ffmpeg -framerate 30 -pattern_type glob -i './frames/*.png' \
        -r 30 -f mp4 -vcodec libx264 -pix_fmt yuv420p \
        ./out/figure_1.7_regularization_variation.mp4
