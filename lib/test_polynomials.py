d0c27d2b995beeab577ee15cf65c1d4a887cb145
Nikolas Schnellbächer <khx0@posteo.net>
2019-04-07 22:57:56 +0200

    updated test_polynomials.py

--- lib/test_polynomials.py
+++ lib/test_polynomials.py
@@ -3,7 +3,7 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2019-03-18
+# date: 2019-04-05
 # file: test_polynomials.py
 # tested with python 2.7.15
 # tested with python 3.7.2
@@ -19,6 +19,8 @@ Unit test invocation:
 Run this test script by calling in this scripts containing directory.
 $python -m pytest
 where python is your desired python interpreter.
+To show print statements add the -s flag to the actual pytest command.
+$python -m pytest -s
 '''
 
 def test_01():
@@ -102,48 +104,52 @@ def test_04():
     res = polynomial_horner(xVals, *coeff)
     
     assert np.array_equal(res, reference)
-    
+
 if __name__ == '__main__':
     
-    pass
-    
-    ######################################################################################
-    # ToDo Clean up below:    
+    test_01()
+    test_02()
+    test_03()
+    test_04()
+
+
+
+
+    nVals = 120
+    xVals = np.linspace(0.0, 1.0, nVals)
+    coeff = np.array([0.0, 1.0])
+    yVals = polynomial_horner(xVals, *coeff)
+    assert xVals.shape == yVals.shape
+    assert np.array_equal(xVals, yVals)
+
+
+
+
+
+
+    # polynomial_horner(Xt)
+
+    # popt, pcov = curve_fit(polynomial_horner, Xt[:, 0], Xt[:, 1], p0 = w)
     
     # test the two different function calls as below
     
-    # test vectorized function calls
-    
-    
-    '''
-    ######################################################################################
-    # global parameters
-    nTrain = 15
-    
-    mu = 0.0
-    sigma = 0.3
-    
-    ######################################################################################
-    # create training data
-    Xt = np.zeros((nTrain, 2))
-    xVals = np.linspace(0.0, 1.0, nTrain)
-    yVals = np.sin(2.0 * np.pi * xVals) + np.random.normal(mu, sigma, xVals.shape)
-    Xt[:, 0] = xVals
-    Xt[:, 1] = yVals
-    
-    ######################################################################################
-    # polynomial curve fitting (learning the model)
-    m = 9
-    w = np.ones((m + 1,))
-    popt, pcov = curve_fit(polynomial_horner, Xt[:, 0], Xt[:, 1], p0 = w)
-    
-    
+    '''        
     # create fitted model
     nModelPoints = 800
     Xm = np.zeros((nModelPoints, 2))
     xVals = np.linspace(0.0, 1.0, nModelPoints)
     yVals = np.zeros_like(xVals)
-    
-    
     yVals = np.array([polynomial_horner(x, *popt) for x in xVals])
     '''
+
+
+
+
+
+
+
+
+
+
+
+
