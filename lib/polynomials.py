09465acd32dffa9da3bb830fbf830ce61f574493
Nikolas Schnellbächer <khx0@posteo.net>
2019-04-08 08:55:55 +0200

    removed trailing whitespace

--- lib/polynomials.py
+++ lib/polynomials.py
@@ -3,7 +3,7 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2019-03-18
+# date: 2019-04-08
 # file: polynomials.py
 # tested with python 2.7.15
 # tested with python 3.7.2
@@ -17,15 +17,15 @@ def polynomial_horner(x, *coeff):
     coeff is a tuple which contains the polynomials coefficients and returns
     f(x) = coeff[0] * x + coeff[1] * x^2 + ... +  coeff[m] * x^m
     where coeff contains the (m + 1) coefficients for a polynomial of degree m.
-    
+
     The functions preserves the input shape, such that
     res.shape == x.shape.
     If x is a pure scalar float, res will equally be a single scalar float.
-    
+
     For ultimate performance you might want to
     overload this function having an explicit function for pure floats
     and for vectorized inputs to avoid the try/except block of this implementation.
-    
+
     The function signature is compatible with scipy's curve_fit module, in the sense
     that polynomial_horner is a callable function which satisfies the notion
     ydata = f(xdata, *params)
