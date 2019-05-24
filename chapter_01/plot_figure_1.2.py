fdee1fcc64f65477ccd26917d8d36292fff2cb9a
Nikolas Schnellbächer <khx0@posteo.net>
2019-05-24 23:17:09 +0200

    upadted to mpl 3.1.0

--- chapter_01/plot_figure_1.2.py
+++ chapter_01/plot_figure_1.2.py
@@ -3,10 +3,10 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2019-04-10
+# date: 2019-05-24
 # file: plot_figure_1.2.py
 # tested with python 2.7.15 in conjunction with mpl version 2.2.3
-# tested with python 3.7.2  in conjunction with mpl version 3.0.3
+# tested with python 3.7.2  in conjunction with mpl version 3.1.0
 ##########################################################################################
 
 import os
@@ -60,7 +60,7 @@ def Plot(titlestr, X, Xt, outname, outdir, pColors,
 
     mpl.rc('font', **{'size': 10})
     mpl.rc('legend', **{'fontsize': 7.0})
-    mpl.rc("axes", linewidth = 0.5)
+    mpl.rc('axes', linewidth = 0.5)
 
     # mpl.rc('font', **{'family' : 'sans-serif', 'sans-serif' : ['Myriad Pro']})
     # mpl.rc('font', **{'family' : 'sans-serif', 'sans-serif' : ['Helvetica']})
