e350120b0fdbef6c73291c222edddfc2de013f3a
Nikolas Schnellbächer <khx0@posteo.net>
2019-08-25 17:57:09 +0200

    updated figure 1.24 script

--- chapter_01/figure_1.24_assay/plot_figure_1.24.py
+++ chapter_01/figure_1.24_assay/plot_figure_1.24.py
@@ -3,7 +3,7 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2019-08-24
+# date: 2019-08-25
 # file: plot_figure_1.24.py
 # tested with python 2.7.15 in conjunction with mpl version 2.2.3
 # tested with python 3.7.2  in conjunction with mpl version 3.0.3
@@ -18,8 +18,7 @@ from matplotlib.pyplot import legend
 
 from scipy.stats import norm
 
-now = datetime.datetime.now()
-now = "{}-{}-{}".format(now.year, str(now.month).zfill(2), str(now.day).zfill(2))
+today = datetime.datetime.now().strftime("%Y-%m-%d")
 
 BASEDIR = os.path.dirname(os.path.abspath(__file__))
 RAWDIR = os.path.join(BASEDIR, 'raw')
@@ -315,7 +314,7 @@ def Plot(titlestr, X, outname, outdir, pColors,
     ######################################################################################
     # save to file
     if datestamp:
-        outname += '_' + now
+        outname += '_' + today
     if savePDF:
         f.savefig(os.path.join(outdir, outname) + '.pdf', dpi = 300, transparent = True)
     if savePNG:
