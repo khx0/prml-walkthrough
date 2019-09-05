cef355790582eac0aadf208fa50feba7c9d394d1
Nikolas Schnellbächer <khx0@posteo.net>
2019-09-05 20:18:11 +0200

    updated mpl routines

--- chapter_01/figure_1.17_bayesian_polynomial_curve_fitting/run_sequential_N_10_colors_mk2.py
+++ chapter_01/figure_1.17_bayesian_polynomial_curve_fitting/run_sequential_N_10_colors_mk2.py
@@ -3,10 +3,9 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2019-04-92
+# date: 2019-09-05
 # file: run_sequential_N_10_colors_mk2.py
-# tested with python 2.7.15 using matplotlib 2.2.3
-# tested with python 3.7.2  using matplotlib 3.0.3
+# tested with python 3.7.2  using matplotlib 3.1.1
 ##########################################################################################
 
 import os
@@ -18,8 +17,7 @@ from matplotlib.pyplot import legend
 
 from bayesianPolyCurveFit import bayesianPolyCurveFit
 
-now = datetime.datetime.now()
-now = "{}-{}-{}".format(now.year, str(now.month).zfill(2), str(now.day).zfill(2))
+today = datetime.datetime.now().strftime("%Y-%m-%d")
 
 BASEDIR = os.path.dirname(os.path.abspath(__file__))
 RAWDIR = os.path.join(BASEDIR, 'raw')
@@ -200,14 +198,14 @@ def Plot(titlestr, X_gt, Xt, Xm, outname, outdir, pColors,
     if grid:
         ax1.grid(color = 'gray', linestyle = '-', alpha = 0.2, which = 'major',
                  linewidth = 0.2)
-        ax1.grid('on')
+        ax1.grid(True)
         ax1.grid(color = 'gray', linestyle = '-', alpha = 0.05, which = 'minor',
                  linewidth = 0.1)
-        ax1.grid('on', which = 'minor')
+        ax1.grid(True, which = 'minor')
     ######################################################################################
     # save to file
     if datestamp:
-        outname += '_' + now
+        outname += '_' + today
     if savePDF:
         f.savefig(os.path.join(outdir, outname) + '.pdf', dpi = 300, transparent = True)
     if savePNG:
