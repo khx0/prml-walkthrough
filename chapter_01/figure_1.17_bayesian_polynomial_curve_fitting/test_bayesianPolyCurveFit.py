b9f878ceddcf3895d94ce018cd086c2327036f64
Nikolas Schnellbächer <khx0@posteo.net>
2019-09-05 20:23:47 +0200

    code polish and clean up

--- chapter_01/figure_1.17_bayesian_polynomial_curve_fitting/test_bayesianPolyCurveFit.py
+++ chapter_01/figure_1.17_bayesian_polynomial_curve_fitting/test_bayesianPolyCurveFit.py
@@ -3,9 +3,8 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2019-04-02
+# date: 2019-09-04
 # file: test_bayesianPolyCurveFit.py
-# tested with python 2.7.15
 # tested with python 3.7.2
 ##########################################################################################
 
