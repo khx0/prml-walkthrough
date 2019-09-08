bcdece9016ce8961fb92311708f30b3fc452caaa
Nikolas Schnellbächer <khx0@posteo.net>
2019-09-08 23:27:00 +0200

    updated mpl routines

--- chapter_01/polynomial_least_squares_curve_fitting/plot_figure_1.4_m_9.py
+++ chapter_01/polynomial_least_squares_curve_fitting/plot_figure_1.4_m_9.py
@@ -3,10 +3,9 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2019-04-10
+# date: 2019-09-08
 # file: plot_figure_1.4_m_9.py
-# tested with python 2.7.15 in conjunction with mpl version 2.2.3
-# tested with python 3.7.2  in conjunction with mpl version 3.0.3
+# tested with python 3.7.2  in conjunction with mpl version 3.1.1
 ##########################################################################################
 
 import os
@@ -19,8 +18,7 @@ from matplotlib.pyplot import legend
 
 mpl.ticker._mathdefault = lambda x: '\\mathdefault{%s}'%x
 
-now = datetime.datetime.now()
-now = "{}-{}-{}".format(now.year, str(now.month).zfill(2), str(now.day).zfill(2))
+today = datetime.datetime.now().strftime("%Y-%m-%d")
 
 BASEDIR = os.path.dirname(os.path.abspath(__file__))
 RAWDIR = os.path.join(BASEDIR, 'raw')
@@ -183,14 +181,14 @@ def Plot(titlestr, X, Xt, Xm, outname, outdir, pColors,
     if grid:
         ax1.grid(color = 'gray', linestyle = '-', alpha = 0.2, which = 'major',
                  linewidth = 0.2)
-        ax1.grid('on')
+        ax1.grid(True)
         ax1.grid(color = 'gray', linestyle = '-', alpha = 0.05, which = 'minor',
                  linewidth = 0.1)
-        ax1.grid('on', which = 'minor')
+        ax1.grid(True, which = 'minor')
     ######################################################################################
     # save to file
     if datestamp:
-        outname += '_' + now
+        outname += '_' + today
     if savePDF:
         f.savefig(os.path.join(outdir, outname) + '.pdf', dpi = 300, transparent = True)
     if savePNG:
@@ -233,8 +231,8 @@ if __name__ == '__main__':
 
     outname = 'prml_ch_01_figure_1.4_PRNG-seed_523456789_m_9_fit_polynomial_leastSq'
 
-    xFormat = [-0.05, 1.05, 0.0, 1.1, 1.0, 1.0]
-    yFormat = [-1.35, 1.35, -1.0, 1.1, 1.0, 1.0]
+    xFormat = (-0.05, 1.05, 0.0, 1.1, 1.0, 1.0)
+    yFormat = (-1.35, 1.35, -1.0, 1.1, 1.0, 1.0)
 
     pColors = {'green': '#00FF00',  # neon green
                'blue': '#0000FF',   # standard blue
