20a718b36c866c3c3958971fb1c9146e4efdf916
Nikolas Schnellbächer <khx0@posteo.net>
2020-02-25 23:57:16 +0100

    updated to python 3.7.6

--- lib/legacy.py
+++ lib/legacy.py
@@ -3,10 +3,10 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2019-04-09
+# date: 2020-02-25
 # file: legacy.py
 # tested with python 2.7.15
-# tested with python 3.7.2
+# tested with python 3.7.6
 ##########################################################################################
 
 def ensure_dir(dir):
--- lib/polynomials.py
+++ lib/polynomials.py
@@ -3,10 +3,10 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2019-04-08
+# date: 2020-02-25
 # file: polynomials.py
 # tested with python 2.7.15
-# tested with python 3.7.2
+# tested with python 3.7.6
 ##########################################################################################
 
 import numpy as np
