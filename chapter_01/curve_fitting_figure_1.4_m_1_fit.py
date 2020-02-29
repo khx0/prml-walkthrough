7190bdd685a1ac826faffed8100e5c168a029f5b
Nikolas Schnellbächer <khx0@posteo.net>
2020-02-29 09:43:39 +0100

    improved outname creation

--- chapter_01/curve_fitting_figure_1.4_m_1_fit.py
+++ chapter_01/curve_fitting_figure_1.4_m_1_fit.py
@@ -60,7 +60,6 @@ if __name__ == '__main__':
     X[:, 1] = yVals
 
     ######################################################################################
-    # file i/o
-    outname = '.'.join( filename.split('.')[:-1] ) + '_m_1_fit.txt'
-    print("outname =", outname)
-    # np.savetxt(os.path.join(RAWDIR, outname), X, fmt = '%.8f')
+    # file i/o    
+    outname = os.path.splitext(filename)[0] + '_m_1_fit.txt'
+    np.savetxt(os.path.join(RAWDIR, outname), X, fmt = '%.8f')
