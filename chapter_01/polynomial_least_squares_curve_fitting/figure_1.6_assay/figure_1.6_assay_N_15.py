1a2b288d4b86bc67546c344ce73d9e1fcef6df16
Nikolas Schnellbächer <khx0@posteo.net>
2020-03-03 23:29:08 +0100

    updated to python 3.7.6

--- chapter_01/polynomial_least_squares_curve_fitting/figure_1.6_assay/figure_1.6_assay_N_15.py
+++ chapter_01/polynomial_least_squares_curve_fitting/figure_1.6_assay/figure_1.6_assay_N_15.py
@@ -3,9 +3,9 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2019-09-05
+# date: 2020-03-03
 # file: figure_1.6_assay_N_15.py
-# tested with python 3.7.2 and mpl 3.1.1
+# tested with python 3.7.6 and mpl 3.1.3
 ##########################################################################################
 
 # noise settings
@@ -174,7 +174,7 @@ def Plot(titlestr, X, Xt, Xm, params, outname, outdir, pColors,
 
     ######################################################################################
     # set plot range
-    if (xFormat == None):
+    if xFormat == None:
         pass
     else:
         major_x_ticks = np.arange(xFormat[2], xFormat[3], xFormat[4])
@@ -183,7 +183,7 @@ def Plot(titlestr, X, Xt, Xm, params, outname, outdir, pColors,
         ax1.set_xticks(minor_x_ticks, minor = True)
         ax1.set_xlim(xFormat[0], xFormat[1])
 
-    if (yFormat == None):
+    if yFormat == None:
         pass
     else:
         major_y_ticks = np.arange(yFormat[2], yFormat[3], yFormat[4])
@@ -259,7 +259,7 @@ if __name__ == '__main__':
 
     ######################################################################################
     # file i/o
-    outname = 'figure_1.6_training_data_N_%d_PRNG-seed_%d.txt' %(nTrain, seedValue)
+    outname = f'figure_1.6_training_data_N_{nTrain}_PRNG-seed_{seedValue}.txt'
     np.savetxt(os.path.join(RAWDIR, outname), Xt, fmt = '%.8f')
     ######################################################################################
 
@@ -278,14 +278,14 @@ if __name__ == '__main__':
 
     ######################################################################################
     # file i/o
-    outname = 'figure_1.6_fitted_model_N_%d_PRNG-seed_%d.txt' %(nTrain, seedValue)
+    outname = f'figure_1.6_fitted_model_N_{nTrain}_PRNG-seed_{seedValue}.txt'
     np.savetxt(os.path.join(RAWDIR, outname), X, fmt = '%.8f')
     ######################################################################################
 
     ######################################################################################
     # call the plotting function
 
-    outname = 'figure_1.6_N_%d_PRNG-seed_%d' %(nTrain, seedValue)
+    outname = f'figure_1.6_N_{nTrain}_PRNG-seed_{seedValue}'
 
     xFormat = (-0.05, 1.05, 0.0, 1.1, 1.0, 1.0)
     yFormat = (-1.5, 1.5, -1.0, 1.1, 1.0, 1.0)
