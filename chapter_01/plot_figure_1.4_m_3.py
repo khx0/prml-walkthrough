7f9e5030c04e112aa82001e2c1ae7eae7cacd700
Nikolas Schnellbächer <khx0@posteo.net>
2020-03-07 23:07:05 +0100

    updated to mpl 3.2.0

--- chapter_01/plot_figure_1.4_m_3.py
+++ chapter_01/plot_figure_1.4_m_3.py
@@ -3,9 +3,9 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2020-02-25
+# date: 2020-03-07
 # file: plot_figure_1.4_m_3.py
-# tested with python 3.7.6 in conjunction with mpl version 3.1.3
+# tested with python 3.7.6 in conjunction with mpl version 3.2.0
 ##########################################################################################
 
 import os
@@ -154,7 +154,7 @@ def Plot(titlestr, X, Xt, Xm, outname, outdir, pColors,
 
     ######################################################################################
     # set plot range
-    if (xFormat == None):
+    if xFormat == None:
         pass
     else:
         major_x_ticks = np.arange(xFormat[2], xFormat[3], xFormat[4])
@@ -163,7 +163,7 @@ def Plot(titlestr, X, Xt, Xm, outname, outdir, pColors,
         ax1.set_xticks(minor_x_ticks, minor = True)
         ax1.set_xlim(xFormat[0], xFormat[1])
 
-    if (yFormat == None):
+    if yFormat == None:
         pass
     else:
         major_y_ticks = np.arange(yFormat[2], yFormat[3], yFormat[4])
@@ -236,8 +236,8 @@ if __name__ == '__main__':
 
     outname = 'prml_ch_01_figure_1.4_PRNG-seed_523456789_m_3_fit'
 
-    xFormat = [-0.05, 1.05, 0.0, 1.1, 1.0, 1.0]
-    yFormat = [-1.35, 1.35, -1.0, 1.1, 1.0, 1.0]
+    xFormat = (-0.05, 1.05, 0.0, 1.1, 1.0, 1.0)
+    yFormat = (-1.35, 1.35, -1.0, 1.1, 1.0, 1.0)
 
     # plot color dictionary
     pColors = {'green': '#00FF00', # neon green
