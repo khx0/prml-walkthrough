6905f61161dc93b4cea12ba675b41bcbb8649847
Nikolas Schnellbächer <khx0@posteo.net>
2020-03-07 11:05:54 +0100

    updated to mpl 3.2.0

--- chapter_01/polynomial_least_squares_curve_fitting/figure_1.6_assay/figure_1.6_assay_sweep.py
+++ chapter_01/polynomial_least_squares_curve_fitting/figure_1.6_assay/figure_1.6_assay_sweep.py
@@ -3,9 +3,9 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2020-03-03
+# date: 2020-03-06
 # file: figure_1.6_assay_sweep.py
-# tested with python 3.7.6 and mpl 3.1.3
+# tested with python 3.7.6 and mpl 3.2.0
 ##########################################################################################
 
 # noise settings
