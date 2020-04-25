2ba430629f37a69dc2d398b1d34e89f20a4b8e9a
Nikolas Schnellbächer <khx0@posteo.net>
2020-04-25 13:36:59 +0200

    clean up figure 1.30 script

--- chapter_01/figure_1.30_assay/plot_figure_1.30.py
+++ chapter_01/figure_1.30_assay/plot_figure_1.30.py
@@ -175,7 +175,7 @@ def Plot(bins, values, outname, outdir, pColors, labelString = None,
 
 def entropy(x):
     '''
-    Computes the discrete entropy of the given
+    Computes the discrete sample entropy of the given
     probabilities x.
     '''
     H = 0.0
@@ -185,20 +185,6 @@ def entropy(x):
         else:
             H += -pi * np.log(pi)
     return H
-
-    # return -x.dot(np.log(x))
-    
-    # make this below a unit test
-    #     x = np.array([0.2, 0.2, 0.2, 0.2, 0.2])
-# 
-#     # x = np.array([0.5, 0.5])
-# 
-#     print(np.sum(x))
-#     
-#     H = entropy(x)
-#     
-#     print("H =", H)
-#     print(np.log(5))
 
 ######################################################################################
 # sample probability values from normal distribution
