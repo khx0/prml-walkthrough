ca5aa5ef236d4928c572809b0bc0531b1909f2f3
Nikolas Schnellbächer <khx0@posteo.net>
2020-05-03 10:59:18 +0200

    updated unit test

--- chapter_01/figure_1.23_assay/test_unitSphereArea.py
+++ chapter_01/figure_1.23_assay/test_unitSphereArea.py
@@ -3,13 +3,13 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: khx0@posteo.net
-# date: 2020-03-02
+# date: 2020-05-03
 # file: test_unitSphereArea.py
 # tested with python 3.7.6
 ##########################################################################################
 
 '''
-pytest invocation (tested with pytest 5.3.5)
+pytest invocation (tested with pytest 5.4.1)
 $cd /path/to/dir/containing/this/script
 $python -m pytest (-v)
 where python is the desired python interpreter of your choice.
