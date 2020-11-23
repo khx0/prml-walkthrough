966883ff6c76186a834229e6be40cb5e02148156
Nikolas Schnellbächer <khx0@posteo.net>
2020-11-23 13:56:29 +0100

    updated plot_figure_1.4_m_1.py

--- chapter_01/plot_figure_1.4_m_1.py
+++ chapter_01/plot_figure_1.4_m_1.py
@@ -5,7 +5,7 @@
 # contact: khx0@posteo.net
 # date: 2020-11-23
 # file: plot_figure_1.4_m_1.py
-# tested with python 3.7.6 in conjunction with mpl version 3.3.
+# tested with python 3.7.6 in conjunction with mpl version 3.3.3
 ##########################################################################################
 
 import os
